156765c4815122f020aaf1d29a627cf89a536819
jaalvaradoh <jaalvaradoh@isil.pe>
2025-11-27 12:04:39 -0500

    Update iris.py

--- iris.py
+++ iris.py
@@ -18,6 +18,47 @@ tree_model = joblib.load('./model/modelo_iris_arbol.pkl')
 # Interfaz
 # ============================
 st.title("Predicción del dataset Iris")
+
+st.info("Autor: Dr. Jesus Alvarado Huayhuaz")
+
+st.sidebar.image("imagenes/iris_dataset.png",
+                 caption="Aplicación de modelo de clasificación en Streamlit")
+
+st.write("""
+El dataset Iris es uno de los conjuntos de datos más conocidos y utilizados en estadística, reconocimiento de patrones e inteligencia artificial. Fue introducido por el botánico y estadístico Ronald A. Fisher en 1936, en su artículo “The Use of Multiple Measurements in Taxonomic Problems”. Su objetivo original era demostrar el uso del análisis discriminante para clasificar especies de plantas a partir de mediciones morfológicas.
+
+El dataset contiene un total de 150 muestras de flores de iris, divididas equitativamente en tres especies:
+
+- Iris setosa
+
+- Iris versicolor
+
+- Iris virginica
+
+Para cada flor se registraron cuatro características morfométricas, medidas en centímetros:
+
+- Longitud del sépalo (sepal length)
+
+- Ancho del sépalo (sepal width)
+
+- Longitud del pétalo (petal length)
+
+- Ancho del pétalo (petal width)
+
+Estas mediciones fueron tomadas originalmente en la década de 1930 a partir de especímenes reales recolectados para estudios botánicos. Fisher utilizó este conjunto para ilustrar cómo las variables cuantitativas permiten separar estadísticamente especies a través de técnicas de clasificación.
+Actualmente, el dataset Iris es ampliamente usado para:
+
+1. Enseñanza de aprendizaje supervisado,
+
+2. Pruebas de modelos de clasificación,
+
+3. Demostraciones de técnicas estadísticas,
+
+4. Ejercicios iniciales de machine learning y visualización de datos.
+
+Su simplicidad, tamaño reducido y separabilidad parcial entre clases lo convirtieron en un estándar académico para introducir conceptos clave de la inteligencia artificial y el reconocimiento de patrones.
+""")
+
 st.write("Ingresa las características de la flor:")
 
 # Campos de entrada numéricos
